66174fe62e0e6950e55f4982900a86562e7ac299
Алексей Могильниченко <aleksejmogilnicenko@MacBook-Pro-Aleksei.local>
2026-09-07 20:49:14 +0800

    deploy

--- config.py
+++ config.py
@@ -1,16 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 STORMGLASS_API_KEY = os.getenv("STORMGLASS_API_KEY")
-
-print("TOKEN:", TELEGRAM_TOKEN)
-print("STORM:", STORMGLASS_API_KEY)
-
-if not TELEGRAM_TOKEN:
-    raise ValueError("TELEGRAM_TOKEN is not set")
-
-if not STORMGLASS_API_KEY:
-    raise ValueError("STORMGLASS_API_KEY is not set")
